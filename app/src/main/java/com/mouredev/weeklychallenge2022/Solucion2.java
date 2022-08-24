be51e089cb84a3a8ea83fce9a10045436a71d501
JuviAu_23 <damjvila@gmail.com>
2022-08-24 14:37:47 +0200

    Solución Reto #2 Java

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Solucion2.java
@@ -0,0 +1,33 @@
+package com.mouredev.weeklychallenge2022;
+/*
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 17/01/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+public class Solucion2 {
+    public static void main(String[] args) {
+        fibonacci(0,1,0);
+    }
+
+    static void fibonacci(long ant1, long ant2,int cont) {
+        long actual = ant1 + ant2;
+        if (cont < 50) {
+            System.out.println(ant1);
+            ant2 = ant1;
+            fibonacci(actual, ant2,cont+1);
+        }
+    }
+}
